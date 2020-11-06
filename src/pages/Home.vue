deecf53061757058e4fb9b86d64fde2f7dde3af2
Kody Dudley <kodymdudley@gmail.com>
2020-11-06 11:31:20 -0700

    checkpoint-5-blogger

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div class="row my-3">
+      <div class="col-12 text-center">
+        <h1>
+          Blogs
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
